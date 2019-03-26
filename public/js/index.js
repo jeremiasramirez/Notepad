14580183c70d11f12c559672565c533f84eb7711
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-26 01:45:52 -0400

    Agregando funcionalidad de eliminar notas y sus respetivos mensajes de advertencias

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,7 @@ class CreateCard{
 		let cardsContainer = document.getElementById("cards");
 		let elementContainer = document.createElement("div");
 			elementContainer.setAttribute("class","elementContainer");
+			elementContainer.setAttribute("id","elementContainerR");
 			cardsContainer.insertAdjacentElement("afterend",elementContainer);
 
 		let titleElement= document.createElement("h1");
@@ -21,7 +22,75 @@ class CreateCard{
 			descriptionElement.textContent = this.description;
 			elementContainer.appendChild(descriptionElement);
 
+			let remov =  document.getElementById("elementContainerR");
+
+		if(remov!==null && remov !== undefined){
+
+			remov.addEventListener("dblclick",(e)=>{
+
+				let valid = document.createElement("div");
+					valid.setAttribute("class","deleteWindow");
+					valid.setAttribute("id","deleteWindow");
+				 
+					setTimeout(()=>{
+						if(valid)valid.remove();
+					},2000)
+
+
+				let yes = document.createElement("button");
+					yes.textContent = " Eliminar ";
+					yes.setAttribute("id","button1Delete");
+					yes.setAttribute("class","button1Delete fas fa-trash-alt ");
+					valid.appendChild(yes);
+
+				let no = document.createElement("button");
+					no.textContent = " Cancelar ";
+					no.setAttribute("class","button2Cancel far fa-window-close");
+					no.setAttribute("id","button2Cancel");
+				 
+					no.setAttribute("id","button2Cancel");
+					valid.appendChild(no);
+
+					let nots = document.getElementById("button2Cancel")
+
+				document.body.insertAdjacentElement("afterbegin",valid);
+
+				yes.addEventListener("click",(e)=>{
+					remov.remove();
+					let msjDeletedExit = document.createElement("p");
+					msjDeletedExit.textContent = " Nota eliminada ";
+					msjDeletedExit.setAttribute("class","messageExit far fa-check-circle");
+					setTimeout(()=>{
+						msjDeletedExit.remove();
+					},1000)
+					document.body.insertAdjacentElement("afterbegin",msjDeletedExit);
+
+					if(valid){
+						setTimeout((e)=>{
+							valid.remove();
+						
+						},0)
+						 
+						
+							
+						 
+						
+					}	
+				},false);
+				no.addEventListener("click",(e)=>{
+					setTimeout((e)=>{
+						valid.remove();
+					},500)
+					 
+				},false)
+				
+			},false)
+		}
+			
+				
+
 	}
+
  
 	createdValidate(){
 		let text = document.createElement("p"); 
@@ -35,8 +104,10 @@ class CreateCard{
 			let time = setTimeout((e)=>{
 
 				text.remove();
-			},2000)
+			},1000)
 	}
+	 
+
 
 
 }
@@ -48,11 +119,16 @@ let buttonSend = document.getElementById("btn__send");
  
 buttonSend.addEventListener("click",(e)=>{
 	if(title.value !== "" && description.value !== "" && title.value.length <= 20){
+		let empty = document.getElementById("empty__notes");
+		if(empty){
+			empty.remove();
+		}
 		let cr = new CreateCard(title.value ,description.value);
 		localStorage.setItem(title.value,description.value);
 
 			cr.createCard();
 			cr.createdValidate();
+			
 			
 			title.value = "";
 			description.value = "";
@@ -60,9 +136,7 @@ buttonSend.addEventListener("click",(e)=>{
 
 },false)
 
-
-
-
+ 
 
 
 
