cf9f1a9a4b6f07efec52fb769c427ef5a0548503
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-27 17:11:42 -0400

    Refactorizando codigo

--- public/js/index.js
+++ public/js/index.js
@@ -1,154 +1,167 @@
 class CreateCard{
-
 	constructor(title,description){
+		// Entrada de datos
 		this.title = title;
 		this.description = description;
-	}
 
+	}
 	createCard(){
-		let cardsContainer = document.getElementById("cards");
-		let elementContainer = document.createElement("div");
+		/*
+		  creando elementos html para la interfaz de la card
+		*/
+		let cardsContainer = document.getElementById("cards"),
+		 		elementContainer = document.createElement("div"),
+				titleElement= document.createElement("h1"),
+				descriptionElement= document.createElement("p");
+
+
 			elementContainer.setAttribute("class","elementContainer");
 			elementContainer.setAttribute("id","elementContainerR");
 			cardsContainer.insertAdjacentElement("afterend",elementContainer);
 
-		let titleElement= document.createElement("h1");
+
 			titleElement.setAttribute("class","titleCard");
-			titleElement.textContent = this.title; 
+			titleElement.textContent = this.title;
 			elementContainer.insertAdjacentElement("afterbegin",titleElement);
 
-		let descriptionElement= document.createElement("p"); 
+
 			descriptionElement.setAttribute("class","descriptionCard")
 			descriptionElement.textContent = this.description;
 			elementContainer.appendChild(descriptionElement);
-
-			let remov =  document.getElementById("elementContainerR");
-
+			let remov = document.getElementById("elementContainerR");
+			// verificando si la card creado existe para poder eliminarla
 		if(remov!==null && remov !== undefined){
 
 			remov.addEventListener("dblclick",(e)=>{
+				let rm = document.getElementById("deleteWindow");
+				if( rm ){
+						document.getElementById("deleteWindow").remove();
+				}
 
-				let valid = document.createElement("div");
-					valid.setAttribute("class","deleteWindow");
-					valid.setAttribute("id","deleteWindow");
-				 
-					setTimeout(()=>{
-						if(valid)valid.remove();
-					},5000)
+					let valid = document.createElement("div");
+									valid.setAttribute("class","deleteWindow");
+									valid.setAttribute("id","deleteWindow");
+									valid.setAttribute("id","deleteWindow");
+
+									setTimeout(()=>{
+										if(valid)valid.remove();
+									},3000)
 
 
-				let yes = document.createElement("button");
+
+
+
+				let yes = document.createElement("button"),
+						no = document.createElement("button"),
+						nots = document.getElementById("button2Cancel"),
+						msjDeletedExit = document.createElement("p");
+
 					yes.textContent = " Eliminar ";
 					yes.setAttribute("id","button1Delete");
 					yes.setAttribute("class","button1Delete fas fa-trash-alt ");
+
 					valid.appendChild(yes);
 
-				let no = document.createElement("button");
 					no.textContent = " Cancelar ";
 					no.setAttribute("class","button2Cancel far fa-window-close");
 					no.setAttribute("id","button2Cancel");
-				 
+
 					no.setAttribute("id","button2Cancel");
 					valid.appendChild(no);
 
-					let nots = document.getElementById("button2Cancel")
- 
 				document.body.insertAdjacentElement("afterbegin",valid);
 
 				yes.addEventListener("click",(e)=>{
+
 					remov.remove();
-					let msjDeletedExit = document.createElement("p");
+
 					msjDeletedExit.textContent = " Nota eliminada ";
 					msjDeletedExit.setAttribute("class","messageExit far fa-trash-alt");
-					 
+
 					setTimeout(()=>{
 						msjDeletedExit.remove();
 					},2500)
-					document.body.insertAdjacentElement("afterbegin",msjDeletedExit);
- 
 
+					document.body.insertAdjacentElement("afterbegin",msjDeletedExit);
 
 					if(valid){
 
 						setTimeout((e)=>{
 							valid.remove();
 						},0)
-						 						 
-						
-					}	
+
+					}
+
 				},false);
 
 				no.addEventListener("click",(e)=>{
 
 					setTimeout((e)=>{
 						valid.remove();
-					},500)
-					 
-				},false)
-				
-			},false)
+				    	},500)
+
+			  	},false)
+
+		  	},false)
 		}
-			
-				
-
-	}
-
- 
-	createdValidate(){
-		let text = document.createElement("p"); 
-			text.textContent = "Haz creado una nota ";
-			let icon = document.createElement("i");
-				icon.setAttribute("class","far fa-check-circle");
-				text.appendChild(icon)
-			text.setAttribute("class","validate");
-			document.body.insertAdjacentElement("afterbegin",text);
-
-			let time = setTimeout((e)=>{
-
-				text.remove();
-			},1000)
-	}
-	 
 
 
 
 }
 
 
-let title = document.getElementById("note__title");
-let description = document.getElementById("note__description");
-let buttonSend = document.getElementById("btn__send");
- 
+	createdValidate(){
+		let text = document.createElement("p"),
+				icon = document.createElement("i");
+			text.textContent = "Haz creado una nota ";
+
+				icon.setAttribute("class","far fa-check-circle");
+
+				text.appendChild(icon)
+				text.setAttribute("class","validate");
+
+			document.body.insertAdjacentElement("afterbegin",text);
+
+			let time = setTimeout((e)=>{
+				text.remove();
+			},1000)
+	}
+}
+
+
+let title = document.getElementById("note__title"),
+			description = document.getElementById("note__description"),
+	 		buttonSend = document.getElementById("btn__send");
+
+(function getData(title,description,buttonSend){
+
 buttonSend.addEventListener("click",(e)=>{
-	if(title.value !== "" && description.value !== "" && title.value.length <= 40){
+
+	if(	(title.value !== "")  && (description.value !== "") && (title.value.length <= 40) ){
+
 		let empty = document.getElementById("empty__notes");
+
 		if(empty){
+
 			empty.textContent += " cargando.. ";
+
 			setTimeout((e)=>{
 				empty.remove();
 			},3000)
-			
+
+
 		}
 		let cr = new CreateCard(title.value ,description.value);
 
 			cr.createCard();
 			cr.createdValidate();
-			
-			
+
+
 			title.value = "";
 			description.value = "";
-		 
-			 
+
 	}
 
 },false)
 
- 
-
-
-
-
-
-
-
-
+})(title,description,buttonSend)
